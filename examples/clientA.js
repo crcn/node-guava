dfd0928feb18b175d2bd27e9ed12214ae47e3029
Craig Condon <craig@spiceapps.com>
2011-08-12 23:55:03 -0500

    dispose listener on first response

--- examples/clientA.js
+++ examples/clientA.js
@@ -6,8 +6,9 @@ var hello_c = connection.collection('hello');
 
 
 //simple enough
-hello_c.on({ name: 'craig' }, function(result)
+var disp = hello_c.on({ name: 'craig' }, function(result)
 {
+	disp.dispose();
 	console.log(result)
 });
 
@@ -16,9 +17,9 @@ hello_c.on({ name: 'tim' }, function(result)
 	console.log(result)
 });
 
-hello_c.on({ name: { $in: ['craig', 'tim' ]}, age: { $gt: 20 }}, function(result)
+/*hello_c.on({ name: { $in: ['craig', 'tim' ]}, age: { $gt: 20 }}, function(result)
 {
 	console.log(result)
-});
+});*/
 
 
