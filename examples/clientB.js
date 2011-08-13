09637b61943b4f4be06c51348ea94cf0be9ead88
Craig Condon <craig@spiceapps.com>
2011-08-12 23:54:50 -0500

    emit multiple of "craig"

--- examples/clientB.js
+++ examples/clientB.js
@@ -7,5 +7,8 @@ var hello_c = connection.collection('hello');
 
 
 hello_c.emit({ name: 'craig', age: 21 });
+hello_c.emit({ name: 'craig', age: 21 });
+hello_c.emit({ name: 'craig', age: 21 });
+hello_c.emit({ name: 'craig', age: 21 });
 hello_c.emit({ name: 'tim', age: 20 });
 
